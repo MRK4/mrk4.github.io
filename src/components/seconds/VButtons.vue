f649b146561008e0be542eb35ce699d8f862089c
MRK4 <markatio77@gmail.com>
2022-06-27 10:47:08 +0200

    Changed contact button style

--- src/components/seconds/VButtons.vue
+++ src/components/seconds/VButtons.vue
@@ -3,9 +3,9 @@
     <div class="rounded-xl">
       <a class="rounded-xl py-4 px-3 flex flex-row items-center h-full w-full bg-blue-500 hover:bg-blue-600 montserrat font-bold text-base text-white shadow-lg shadow-blue-500/50 hover:shadow-blue-600/50" href="https://drive.google.com/file/d/19Yi1fnwWbLRzt0W0yqDdv1i7J97UFoC5/view?usp=sharing">Download CV <i class="ml-2 fa-solid fa-file-arrow-down" alt="îcone de téléchargement"></i></a>
     </div>
-    <div class="flex justify-center items-center h-10 w-10 bg-[#D0D0DA] dark:bg-gray-800 rounded-xl shadow-lg">
+    <div class="flex justify-center items-center h-10 w-10 bg-[#D0D0DA] hover:bg-blue-500 dark:bg-gray-800 hover:dark:bg-blue-500 text-gray-800 hover:text-white dark:text-white rounded-xl shadow-lg">
       <a class="flex justify-center items-center h-full w-full" href="mailto:poudreeclement@gmail.com">
-        <i class="fa-regular fa-envelope fa-lg fill-current dark:text-white" alt="îcone d'enveloppe"></i>
+        <i class="fa-regular fa-envelope fa-lg fill-current" alt="îcone d'enveloppe"></i>
       </a>
     </div>
   </div>
